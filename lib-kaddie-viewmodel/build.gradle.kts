f34462f162c22841524c5d00a7696b357c2ccfd1
DWA <dylan.warlus@spirtech.com>
2025-07-03 14:47:08 +0200

    FIX: update namespace for lib-kaddie-viewmodel

--- lib-kaddie-viewmodel/build.gradle.kts
+++ lib-kaddie-viewmodel/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 val javaVersion: JavaVersion by rootProject.extra
 
 android {
-    namespace = "sh.bitsy.lib.kaddie"
+    namespace = "sh.bitsy.lib.kaddie.viewmodel"
     compileSdk = rootProject.extra["compileSdk"] as Int
 
     defaultConfig {
